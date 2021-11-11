58dc7de97746c5c83e723dc96ea885f3cbdd1712
Sam <RealSamwiseGamgee@protonmail.com>
2021-11-12 00:41:58 +0800

    save

--- programs/course_2/src/lib.rs
+++ programs/course_2/src/lib.rs
@@ -1,9 +1,9 @@
 use anchor_lang::prelude::*;
 
-declare_id!("9eyezNhnXYyEDYeUevEV1KEczoVCYgtVp1EVeas3CBYp");
+declare_id!("CqU8bUXfDFS5fn1W2uJ59Rnpmud5ck8CoWhiskzLJhou");
 
 #[program]
-pub mod course_1 {
+pub mod course_2 {
     use super::*;
     pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult {
         Ok(())
--- tests/course-2/spec.ts
+++ tests/course-2/spec.ts
@@ -1,10 +1,10 @@
 import * as anchor from "@project-serum/anchor";
-import { Course1 } from "../target/types/course_1';";
+import { Course1 } from "../target/types/course_2';";
 
-describe("course 1", () => {
+describe("course 2", () => {
   anchor.setProvider(anchor.Provider.env());
 
-  const program = anchor.workspace.Course1;
+  const program = anchor.workspace.Course2;
 
   it("Initialize", async () => {
     // Simple transaction
