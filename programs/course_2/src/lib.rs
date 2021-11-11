4fc9734ee5d539be7db58ae8baed859cccc492fc
Sam <RealSamwiseGamgee@protonmail.com>
2021-11-12 00:41:58 +0800

    save

--- programs/course_2/src/lib.rs
+++ programs/course_2/src/lib.rs
@@ -1,14 +1,52 @@
 use anchor_lang::prelude::*;
+use std::str::from_utf8;
 
 declare_id!("CqU8bUXfDFS5fn1W2uJ59Rnpmud5ck8CoWhiskzLJhou");
 
 #[program]
 pub mod course_2 {
     use super::*;
-    pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, new_text: Vec<u8>) -> ProgramResult {
+        let announcement = &mut ctx.accounts.announcement;
+        let text = from_utf8(&new_text).map_err(|err| {
+            msg!("Invalid utf-8 encoding from : {}", err.valid_up_to());
+            ProgramError::InvalidInstructionData
+        })?;
+        msg!(text);
+        announcement.text = new_text;
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, new_text: Vec<u8>) -> ProgramResult {
+        let announcement = &mut ctx.accounts.announcement;
+        let text = from_utf8(&new_text).map_err(|err| {
+            msg!("Invalid utf-8 encoding from : {}", err.valid_up_to());
+            ProgramError::InvalidInstructionData
+        })?;
+        msg!(text);
+        announcement.text = new_text;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 256)]
+    pub announcement: Account<'info, Announcement>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub announcement: Account<'info, Announcement>,
+}
+
+#[account]
+pub struct Announcement {
+    pub text: Vec<u8>,
+}
--- tests/course-2/spec.ts
+++ tests/course-2/spec.ts
@@ -1,14 +1,52 @@
+import assert from "assert";
 import * as anchor from "@project-serum/anchor";
 import { Course1 } from "../target/types/course_2';";
 
 describe("course 2", () => {
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.local();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.Course2;
+  let _programAccount: anchor.web3.Keypair;
 
   it("Initialize", async () => {
-    // Simple transaction
-    const tx = await program.rpc.initialize({});
+    const program = anchor.workspace.Course2;
+
+    const programAccount = anchor.web3.Keypair.generate();
+
+    const tx = await program.rpc.initialize(Buffer.from("Hello world!"), {
+      accounts: {
+        announcement: programAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [programAccount],
+    });
     console.log("Your transaction signature", tx);
+
+    // Fetch the newly created account from the cluster.
+    const account = await program.account.announcement.fetch(
+      programAccount.publicKey
+    );
+
+    assert.deepEqual(account.text, Buffer.from("Hello world!"));
+
+    // Store the account for the next test.
+    _programAccount = programAccount;
+  });
+
+  it("Update", async () => {
+    const programAccount = _programAccount;
+    const program = anchor.workspace.Course2;
+    await program.rpc.update(Buffer.from("Hello Solana!"), {
+      accounts: {
+        announcement: programAccount.publicKey,
+      },
+    });
+
+    // Fetch the newly created account from the cluster.
+    const account = await program.account.announcement.fetch(
+      programAccount.publicKey
+    );
+    assert.deepEqual(account.text, Buffer.from("Hello Solana!"));
   });
 });
